4eaa0e568e53439199cb8e748a08b351abcff77c
EasonWQY <qw416@nyu.edu>
2016-03-28 16:05:27 -0400

    homework 6

---
+++ Programming assignment 6/stories.blade.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+    </head>
+    <body>
+        @foreach ($stories as $story)
+        <p> {{ $story->title}}  </p>
+        <ol> 
+            @foreach($story->tags as $tag)
+                <li>{{$tag->value}} </li>
+                @endforeach
+        </ol>
+        <p> {{ $story->story}}  </p>
+        <p> {{ $story->published}} </p>
+        @endforeach
+    </body>
+</html>
