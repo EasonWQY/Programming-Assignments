6007dd7e920926a66317200cdba9680948a2693c
EasonWQY <qw416@nyu.edu>
2016-04-04 13:09:30 -0400

    Assignment 7

---
+++ Programming Assignments 7/StoryController.php
@@ -0,0 +1,43 @@
+<?php
+	namespace App\Http\Controllers;
+	use Illuminate\Http\Request;
+	use DB;
+	use App\Http\Requests;
+	use App\Http\Controllers\Controller;
+	use App\Story;
+	use App\Tag;
+	use App\Location;
+	
+	class StoryController  extends Controller {
+		
+		public function index()
+		 {
+			$stories = Story::with('tags')->get();
+		  return view('stories' , ['stories' => $stories]);
+			
+         }
+        public function tags()
+    {         
+	      $tags = Tag::all();
+          return view('tags', ['tags' => $tags]);
+	
+	}
+	    public function create()
+	{
+	    $location = Location::all();
+	      return view('story.create', ['locations' => $location]);	
+	}
+	
+	    public function store(Request $request)
+	{
+		DB::table('stories')->insert([
+		'title' => $request->input('title'),
+		'story' => $request->input('story'),
+		'published' => $request->input('published'),
+		'location_id' => $request->input('location_id'),
+		]);
+	}
+
+	        
+}
+?>
